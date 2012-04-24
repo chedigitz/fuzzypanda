5ab17c5979d17642339b8e582c2a8dddaf8cd74f
Juan Vasquez <jvasquez11@gmail.com>
2012-04-24 03:38:38 -0400

    mongodq database

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,6 @@
 case Padrino.env
   when :development then MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
-  when :production then MongoMapper.connection = Mongo::Connection.new('mongodb://chedigitz:youknow11@flame.mongohq.com:27103/pf_test', nil, :logger => logger)
+  when :production then MongoMapper.connection = Mongo::Connection.new('mongodb://chedigitz:youknow11@flame.mongohq.com', 27103, :logger => logger)
 end
 case Padrino.env
   when :development then MongoMapper.database = 'jp2_development'
