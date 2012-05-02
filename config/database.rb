ca3c10bbf8ca9534c95bad774d1a92e3ade34888
Juan Vasquez <jvasquez11@gmail.com>
2012-05-01 23:58:49 -0400

    mongo conenction in produciton uri changes

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,9 @@
+
+
+
 case Padrino.env
   when :development then MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
-  when :production then MongoMapper.connection = Mongo::Connection.from_uri('mongodb://chedigitz:welcome11@flame.mongohq.com:27103', :logger => logger).db('pf_test')
+  when :production then MongoMapper.connection = Mongo::Connection.from_uri('mongodb://[chedigitz:welcome11@]flame.mongohq.com:27103',  :logger => logger).db('pf_test')
 
 end
 case Padrino.env
