c48f15a2aac7e386ab048d60eef6b6d2e0dc79d6
Juan Vasquez <jvasquez11@gmail.com>
2012-05-01 23:27:39 -0400

    fix production datauri

--- config/database.rb
+++ config/database.rb
@@ -1,9 +1,10 @@
 case Padrino.env
   when :development then MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
-  when :production then MongoMapper.connection = Mongo::Connection.from_uri('mongodb://chedigitz:welcome11@flame.mongohq.com', 27103,  :logger => logger).db('pf_test')
+  when :production then MongoMapper.connection = Mongo::Connection.from_uri('mongodb://chedigitz:welcome11@flame.mongohq.com:27103', :logger => logger).db('pf_test')
 
 end
 case Padrino.env
   when :development then MongoMapper.database = 'jp2_development'
+  when :production  then MongoMapper.database = 'pf_test'
   when :test        then MongoMapper.database = 'jp2_test'
 end
