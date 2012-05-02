4d1dd598d6c1bf34dcd94abd4db4db01e62ebbc5
Juan Vasquez <jvasquez11@gmail.com>
2012-05-02 01:30:36 -0400

    added yaml config file for database

--- config/database.rb
+++ config/database.rb
@@ -1,16 +1,11 @@
 
-case Padrino.env
-  when :development then MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
-  when :production then 
-  	MongoMapper.config = { 
-  		:production => { 'uri' => 'mongodb://chedigitz:welcome11@flame.mongohq.com:27103/pf_test'},
-  	    :test => { 'uri' => 'mongodb://localhost/jp2_test'},
-  	    :development => {uri => 'mongodb://localhost/jp2_development'}
-  	}
-    MongoMapper.connect(Padrino.env)
-end
-case Padrino.env
-  when :development then MongoMapper.database = 'jp2_development'
-  when :production  then MongoMapper.database = 'pf_test'
-  when :test        then MongoMapper.database = 'jp2_test'
-end
+require 'yaml'
+
+configuration = YAML::load(File.open(File.join(PADRINO_ROOT,"config", '.mongo.yml')))
+
+
+#if ENV['HEROKU']
+  #PADRINO_ENV = "heroku"
+#end
+
+MongoMapper.setup(configuration, PADRINO_ENV, :logger => logger, :slave_ok => true)
