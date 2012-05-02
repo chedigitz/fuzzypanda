0c4c2fc0e856d5cebc39c2eef83441c1692ce321
Juan Vasquez <jvasquez11@gmail.com>
2012-05-02 00:25:24 -0400

    fixing mongo uri

--- config/database.rb
+++ config/database.rb
@@ -2,11 +2,9 @@
 
 
 case Padrino.env
-  when :development then
-   MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
-  when :production then
-   MongoMapper.connection = Mongo::Connection.from_uri('mongodb://flame.mongohq.com:27103',  :logger => logger).db('pf_test')
-   
+  when :development then MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
+  when :production then MongoMapper.connection = Mongo::Connection.from_uri('mongodb://chedigitz:welcome11@flame.mongohq.com:27103/',  :logger => logger).db('pf_test')
+
 end
 case Padrino.env
   when :development then MongoMapper.database = 'jp2_development'
