5be7a0790a4c126f158d6cba2167a9ecb67f0891
Juan Vasquez <jvasquez11@gmail.com>
2012-05-05 02:40:18 -0400

    patched dashboard list order

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -2,7 +2,7 @@ Admin.controllers :base do
 
   get :index, :map => "/" do
   	if current_account.role == 'crew'
-      @callsheets = Callsheet.where(:conditions => {:call_time.gte =>Time.now, :account_ids => current_account.id }).order(:call_time.desc)
+      @callsheets = Callsheet.where(:conditions => {:call_time.gte =>Time.now, :account_ids => current_account.id }).order(:call_time.asc)
   	  logger.info "THIS IS OPEN ASSIGMENTS = #{@callsheets.to_json}"
       @un_assigments= Assigment.all(:conditions => {:account_id => current_account.id, :settled => false})
       logger.info "this is UNPAID = #{@un_assigments.to_json}" 
