8f6bce986372efaaee2b616d80aaeeeeec008bdd
Juan Vasquez <jvasquez11@gmail.com>
2012-05-08 04:24:06 -0400

    fb canvas auth

--- app/controllers/fb.rb
+++ app/controllers/fb.rb
@@ -21,13 +21,29 @@ Jp2.controllers :fb do
 
 
   post :index do
-     @oauth = Koala::Facebook::OAuth.new(FB_APP_ID, FB_SECRET_KEY)
-     @signed_request_string = @oauth.parse_signed_request(params['signed_request'])
-     @events = Event.all(:order => 'created_at asc', :limit => 5)
-     @videos = gfl_url_for("promo", @events)
-     # @videos = @events.map { |event| 'http://gdl.gfl.tv/video/eventpromo/' + event.gfl_id.to_s + '.mp4' }
-     
-     render 'fb/index' , :layout => false
+     @oauth = Koala::Facebook::OAuth.new(FB_APP_ID, FB_SECRET_KEY, 'https://purplepanda.heroku.com/fb/authenticate')
+     @signed_request = @oauth.parse_signed_request(params['signed_request'])
+     auth = Account.where("authentications.provider" => 'Facebook', "authentications.uid" => signed_request['user_id']).first
+
+     if auth
+       
+       
+       # @videos = @events.map { |event| 'http://gdl.gfl.tv/video/eventpromo/' + event.gfl_id.to_s + '.mp4' }
+       set_current_account(auth)
+       
+
+      else
+      
+        #new user that is not logged in and has no authentications on file 
+        
+        logger.info "user not logged nor is auth valid"
+        redirect @oauth.url_for_auth_code(:permissions => "publish_stream")
+
+      end
+      @events = Event.all(:order => 'created_at asc', :limit => 5)
+      @videos = gfl_url_for("promo", @events)
+      render 'fb/index' , :layout => false 
+
 
   end
 
