655d2c7c8fec10b552b9f7f8654ed43ec2848be2
Juan Vasquez <jvasquez11@gmail.com>
2012-05-09 06:44:25 -0400

    facebook canvas auth

--- app/controllers/fb.rb
+++ app/controllers/fb.rb
@@ -20,6 +20,7 @@ Jp2.controllers :fb do
 
 
 
+
   post :index do
      unless logged_in?
        @oauth = Koala::Facebook::OAuth.new(FB_APP_ID, FB_SECRET_KEY, 'https://purplepanda.heroku.com/fb/authenticate/')
@@ -27,26 +28,20 @@ Jp2.controllers :fb do
        auth = Account.where("authentications.provider" => 'facebook', "authentications.uid" => @signed_request['user_id']).first
 
        if auth
-       
+        #if authentication exist log user set the current account to the fb uid
        
        # @videos = @events.map { |event| 'http://gdl.gfl.tv/video/eventpromo/' + event.gfl_id.to_s + '.mp4' }
          set_current_account(auth)
-       
-
-        else
-      
+       else
         #new user that is not logged in and has no authentications on file 
-        
-          logger.info "user not logged nor is auth valid"
-          redirect @oauth.url_for_oauth_code(:permissions => "publish_stream, publish_checkins, rsvp_event, create_event, user_events, user_location, email, publish_actions, user_actions.video")
+        logger.info "user not logged nor is auth valid"
+        redirect @oauth.url_for_oauth_code(:permissions => "publish_stream, publish_checkins, rsvp_event, create_event, user_events, user_location, email, publish_actions, user_actions.video")
 
-        end
+       end
      end
         @events = Event.all(:order => 'created_at asc', :limit => 5)
         @videos = gfl_url_for("promo", @events)
         render 'fb/index' , :layout => false 
-
-
      
   end
 
@@ -67,24 +62,27 @@ Jp2.controllers :fb do
   end
 
   post :authenticate do 
-     omniauth = request.env["omniauth.auth"]
-     logger.info  omniauth
-     logger.info "user not logged nor is auth valid"
-     auth = Account.where("authentications.provider" => omniauth['provider'], "authentications.uid" => omniauth['uid']).first
+     @oauth = Koala::Facebook::OAuth.new(FB_APP_ID, FB_SECRET_KEY, 'https://purplepanda.heroku.com/fb/authenticate/')
+     code = params['code']
+     oauth_token = @oauth.fetch_token_string(code)
+     graph = Koala::Facebook::API.new(oauth_token)
+     user_data = graph.get_object('me')
+     user_picture = graph.get_picture('me')
+     
+     auth = Account.where("authentications.provider" => 'facebook', "authentications.uid" => user_data['id']).first
      if auth 
         #existing account update permisions
         credentials = Hash.new
-        credentials['provider'] = omniauth['provider']
-        credentials['uid'] = omniauth['uid']
-        credentials['info'] = omniauth['info']
-        credentials['credentials'] = omniauth['credentials']
+        credentials['provider'] = 'facebook'
+        credentials['uid'] = user_data['id']
+        credentials['info'] = user_data
         logger.info "saved credentials #{credentials.to_json}"
         logger.info omniauth.to_json
         current_account.authentications.create(credentials)
      else
       #new account create account and build authencations 
-       @account = Account.new(:name => omniauth["info"]["name"], :email => omniauth["info"]["email"], :role => "users", :provider => omniauth["provider"], :uid => omniauth["uid"])
-       @account.authentications.build(:provider => omniauth["provider"], :uid => omniauth["uid"])
+       @account = Account.new(:name => user_data['first_name'], :surname => user_data['last_name'], :email => user_data["email"], :role => "users", :provider => 'facebook', :uid => user_data['id'])
+       @account.authentications.build(:provider => 'facebook', :uid => user_data['id'])
        @account.save!
        set_current_account(@account)
      end 
