0bf5770e23bc4421412a7d6c4a7872f1ce4d9089
Juan Vasquez <jvasquez11@gmail.com>
2012-05-09 22:42:54 -0400

    fixing finder bug

--- app/controllers/orders.rb
+++ app/controllers/orders.rb
@@ -74,7 +74,7 @@ Jp2.controllers :orders do
       auth = Authentication.find_by_uid(buyer_id)
       event = Event.find(order_info)
       logger.info "Account = #{auth.account.to_json}"
-      logger.info "event = #{event.to_json}"
+      logger.info "event = #{event}"
       neworder = Order.new(:event_id => order_info, :account_id => auth.account.id, :pay_provider => "facebook", :fb_order_id => order_id, :status => 'initiated', :token => token)
       
       if event
