455b49d80fe3d2fc4bb3af20b401cfe0d8a9f667
Juan Vasquez <jvasquez11@gmail.com>
2012-05-09 22:01:15 -0400

    fixing order info parsing

--- app/controllers/orders.rb
+++ app/controllers/orders.rb
@@ -69,9 +69,9 @@ Jp2.controllers :orders do
       #retrieve order 
       buyer_id = params["buyer"]
       account = Account.where("authentications.uid" => buyer_id).first     
-      localitem = Event.find_by_id(order_info)
-      neworder = Order.new(:event_id => localitem.id, :account_id => account.id, :pay_provider => "facebook", :fb_order_id => order_id, :status => 'initiated', :token => token)
-      if localitem
+      event = Event.find(order_info)
+      neworder = Order.new(:event_id => order_info, :account_id => account.id, :pay_provider => "facebook", :fb_order_id => order_id, :status => 'initiated', :token => token)
+      if event
         #returns a facebook json item description 
         item = neworder.fb_item_info
 
