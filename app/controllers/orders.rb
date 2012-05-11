4a1621b5e9fefc69e2e59e2ba383c27a6538f8c8
Juan Vasquez <jvasquez11@gmail.com>
2012-05-11 00:15:03 -0400

    adjusting json typos

--- app/controllers/orders.rb
+++ app/controllers/orders.rb
@@ -117,7 +117,9 @@ Jp2.controllers :orders do
       order_details = JSON.parse(@signed_request['credits']['order_details'])
       order_id = order_details['order_id']
       buyer = order_details['buyer']
-      status = JSON.parse(@signed_request['credits']['status'])
+      credits = JSON.parse(@signed_request['credits'])
+      status = credits['status']
+      logger.info "credits = #{credits.to_json}"
       logger.info "order details = #{order_details.to_json}"
       logger.info "order_id = #{order.to_json}"
       logger.info "payment status update #{status}"
